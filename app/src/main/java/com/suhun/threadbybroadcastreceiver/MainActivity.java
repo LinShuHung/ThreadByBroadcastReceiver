81039032090ee24dbc62bf53934b9ca23a34c438
Shu-Hung Lin <suhunlin0310@gmail.com>
2023-06-16 09:31:08 +0800

    Add original code

--- app/src/main/java/com/suhun/threadbybroadcastreceiver/MainActivity.java
+++ app/src/main/java/com/suhun/threadbybroadcastreceiver/MainActivity.java
@@ -6,13 +6,32 @@ import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.IntentFilter;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    private String tag = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    public void createFun(View view){
+        Log.d(tag, "+++++MaiActivity run createFun+++++");
+
     }
 }
