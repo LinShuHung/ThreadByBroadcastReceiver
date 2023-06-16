595dd4590c47842c90a423a823bea234d2012ffa
Shu-Hung Lin <suhunlin0310@gmail.com>
2023-06-16 09:07:24 +0800

    Add original code

---
+++ app/src/main/java/com/suhun/threadbybroadcastreceiver/MainActivity.java
@@ -0,0 +1,18 @@
+package com.suhun.threadbybroadcastreceiver;
+
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
